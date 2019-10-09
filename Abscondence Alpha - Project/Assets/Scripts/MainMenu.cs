9c3332e2a2f15820c447378d987535518eca9db7
Will Hart <clouduniversemc@gmail.com>
2019-10-09 16:22:40 +1100

    Abscondence - Alpha Ver. 1.1
    Main menu added

---
+++ Abscondence Alpha - Project/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public string FirstLevelName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+    public void Play()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(FirstLevelName);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
