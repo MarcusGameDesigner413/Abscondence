5256ae03de1a9b29bc29d8f9ff576dbf3a20d7bc
Will Hart <clouduniversemc@gmail.com>
2019-10-14 10:03:20 +1100

    Abscondence - Alpha Ver. 1.2
    -Player Updated
    -Teleport Updated
    -Pause Script added

--- Abscondence Alpha - Project/Assets/Scripts/PlayerController.cs
+++ Abscondence Alpha - Project/Assets/Scripts/PlayerController.cs
@@ -3,35 +3,52 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
-
-    public float walkSpeed = 7;
-    public float runSpeed = 10;
-
+    public int currentHealth = 100;
+    public int maxHealth = 100;
+    public float walkSpeed = 5;
+    public float runSpeed = 10; // For Debug purposes [REMOVE IN ALPHA]
     public float turnSmoothTime = 0.1f;
-    float turnSmoothVelocity;
-
     public float speedSmoothTime = 0.1f;
+    public float invulnerabilityTime = 0.5f;
+    public float knockBackForce;
+    public float knockBackTime;
+    private float knockBackCounter;
+    public float slowDownAmount = 0.2f;
+
+    float turnSmoothVelocity;
     float speedSmoothVelocity;
     float currentSpeed;
 
-    public int currentHealth = 75;
-    public int maxHealth = 100;
+    public GameObject meleeWeapon;
+    Animator meleeSwipe;
+
+    private CharacterController controller;
+    private CapsuleCollider playerCollider;
+    private Vector3 playerMoveDirection;
+    private bool playerWasDamaged;
+    private float timer = 0;
+    private Vector3 velocity;
+    private float startingHeight;
+
+    [HideInInspector]
+    public bool gamePaused;
 
     public int storedPowerCell = 0;
     public int maxPowerCell = 5;
 
-    public GameObject meleeWeapon;
-
-    Animator meleeSwipe;
-    bool moving = false;
-
     void Start()
     {
         meleeSwipe = meleeWeapon.GetComponent<Animator>();
+        playerCollider = GetComponent<CapsuleCollider>();
+        controller = GetComponent<CharacterController>();
+        startingHeight = transform.position.y;
     }
 
     void Update()
     {
+        // Ignore the collisions between the sword and the environment (mostly the enemy cause it would damage him)
+        Physics.IgnoreLayerCollision(0, 9, true);
+
         // Get the direction of input from the user
         Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         // Normalize the input
@@ -39,12 +56,19 @@ public class PlayerController : MonoBehaviour
 
         if (inputDir != Vector2.zero)
         {
+            /* ***THIS CODE LOCKS THE PLAYER ROTATION WHEN ATTACKING ANIMATION IS PLAYING***
+            if (!(meleeSwipe.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1) && !meleeSwipe.IsInTransition(0))
+            {*/
+
             // Set the target rotation to be equal to the direction that the player is facing
             float targetRotation = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg;
             // Change the rotation to the player to be equal to that direction with smoothing
             transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime);
+
+            //}
         }
 
+        bool movementDisabled = false;
         // Debug addition to get around faster
         bool running = Input.GetKey(KeyCode.LeftShift);
         // Set to walkSpeed in alpha test
@@ -53,26 +77,153 @@ public class PlayerController : MonoBehaviour
         currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
 
         // Move the character relevant to the set current speed
-        transform.Translate(transform.forward * currentSpeed * Time.deltaTime, Space.World);
+        //transform.Translate(transform.forward * currentSpeed * Time.deltaTime, Space.World);
+        if (!movementDisabled)
+            controller.Move(((transform.forward * currentSpeed) + velocity) * Time.deltaTime);
 
-        playAnimation();
+        // Subtract the velocity by the slowDownAmount to slow down the knockback
+        velocity -= velocity * slowDownAmount;
+
+        // If the velocity gets below a certain threshold, set it to zero
+        if (velocity.magnitude < 0.35f)
+            velocity = Vector3.zero;
+
+        // Make sure the health doesn't overcap
+        if (currentHealth > maxHealth)
+            currentHealth = maxHealth;
+
+        // Player takes damage upon falling into hole
+        if (transform.position.y < 0)
+        {
+            transform.position = new Vector3(0, 5, 0);
+            currentHealth -= 50;
+        }
+        else if (transform.position.y > startingHeight) // Make sure the player stays on the ground
+        {
+            var previousX = transform.position.x;
+            var previousZ = transform.position.z;
+
+            transform.position = new Vector3(previousX, startingHeight, previousZ);
+        }
+
+        // Only use the timer if the counter has been activated
+        if (knockBackCounter > 0)
+        {
+            movementDisabled = true;
+            knockBackCounter -= Time.deltaTime;
+        }
+        // Once the Counter reaches 0, removes the force applied to the enemy
+        else if (knockBackCounter <= 0)
+        {
+            movementDisabled = false;
+        }
+
+
+        // Check if player took damage
+        PlayerTookDamage();
+
+        // Check for animation plays
+        PlayLightAnimation();
+        PlayHeavyAnimation();
     }
 
-    public void playAnimation()
+    public void PlayLightAnimation()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && !gamePaused)
         {
-            meleeSwipe.SetTrigger("Active");
+            meleeSwipe.SetTrigger("ActiveLClick");
         }
     }
 
-    //updated with on trigger stay
+    public void PlayHeavyAnimation()
+    {
+        if (Input.GetMouseButtonDown(1) && !gamePaused)
+        {
+            meleeSwipe.SetTrigger("ActiveRClick");
+        }
 
+
+        // ***OLD CODE FOR A CHARGE UP AND RELEASE ANIMATION***
+        // No it doesn't work.
+        /*float holdTimer = 0;
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            // Start the charge up animation
+            meleeSwipe.SetBool("ActiveRClick", true);
+
+            // Once the user has held for 1 second, play the spin animation
+            holdTimer += Time.deltaTime;
+            if (Input.GetMouseButtonUp(1) && holdTimer > 1.0f)
+            {
+                meleeSwipe.SetBool("ActiveRClick", false);
+                meleeSwipe.SetTrigger("ActiveRComplete");
+                holdTimer = 0;
+            }
+            else if (Input.GetMouseButtonUp(1) && holdTimer < 1.0f)
+            {
+                meleeSwipe.SetBool("ActiveRClick", false);
+            }
+        }*/
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        Vector3 playerHitDirection = other.transform.forward /*other.transform.position - transform.position*/;
+        playerHitDirection = playerHitDirection.normalized;
+
+        if (other.gameObject.tag == "Enemy")
+        {
+            playerWasDamaged = true;
+            KnockBack(playerHitDirection);
+            currentHealth -= 10;
+        }
+    }
+
+    void PlayerTookDamage()
+    {
+        // If the enemy took damage turn off the box collider
+        if (playerWasDamaged)
+        {
+            PlayerInvulnerabilityOn();
+            timer += Time.deltaTime;
+        }
+
+        // And turn it back on after half a second (or change to be after the spin attack is finished)
+        if (timer >= invulnerabilityTime)
+        {
+            PlayerInvulnerabilityOff();
+            playerWasDamaged = false;
+        }
+    }
+
+    void PlayerInvulnerabilityOn()
+    {
+        playerCollider.enabled = false;
+        Debug.Log("Collider.enabled = " + playerCollider.enabled);
+    }
+
+    void PlayerInvulnerabilityOff()
+    {
+        playerCollider.enabled = true;
+        timer = 0;
+        Debug.Log("Collider.enabled = " + playerCollider.enabled);
+    }
+
+    public void KnockBack(Vector3 direction)
+    {
+        knockBackCounter = knockBackTime;
+
+        playerMoveDirection = direction * knockBackForce;
+
+        // Apply velocity relative to the direction the player has been knocked back
+        velocity += playerMoveDirection;
+    }
+
+    //updated with on trigger stay
     void OnTriggerStay(Collider collision)
     {
-
-
-        //powercell pickup
+        // Powercell pickup
         if (collision.gameObject.tag == "PowerCell" && Input.GetKeyDown(KeyCode.E))
         {
             if (storedPowerCell >= maxPowerCell)
@@ -87,10 +238,9 @@ public class PlayerController : MonoBehaviour
                 //delete the power cell
                 Destroy(collision.gameObject);
             }
-
         }
 
-        //door open -- this requires the panel object to have the tag 'Panel'
+        // Door open -- this requires the panel object to have the tag 'Panel'
         if (collision.gameObject.tag == "Panel" && Input.GetKeyDown(KeyCode.E))
         {
 
@@ -108,10 +258,7 @@ public class PlayerController : MonoBehaviour
             else
             {
                 //play sound effect of --NO--, DO NOT REMOVE FROM SCORE
-
             }
-
-
         }
 
         //health interact
@@ -142,10 +289,7 @@ public class PlayerController : MonoBehaviour
             else
             {
                 //play sound effect of --NO--, DO NOT REMOVE FROM SCORE
-
             }
-
         }
-
     }
 }
--- Abscondence Alpha - Project/Assets/Scripts/TeleportPlayer.cs
+++ Abscondence Alpha - Project/Assets/Scripts/TeleportPlayer.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class TeleportPlayer : MonoBehaviour
 {
-	public GameObject teleportTarget;
+    public GameObject teleportTarget;
 
-	private void OnTriggerEnter(Collider other)
-	{
+    private void OnTriggerEnter(Collider other)
+    {
+        Vector3 teleportDestination = teleportTarget.transform.position - other.transform.position;
         Debug.Log("God has entered the teleporter");
-            if (other.tag == "Player")
-            {
-                Debug.Log("God has abandoned us");
-                other.GetComponent<Rigidbody>().transform.SetPositionAndRotation(teleportTarget.transform.position, other.transform.rotation);
-            }
-      
-	}
+        if (other.tag == "Player")
+        {
+            Debug.Log("God has abandoned us");
+            other.GetComponent<CharacterController>().enabled = false;
+            other.GetComponent<CharacterController>().transform.SetPositionAndRotation(teleportTarget.transform.position, other.transform.rotation);
+            other.GetComponent<CharacterController>().enabled = true;
+             
+            // CharacterController.Move is off a little bit
+            //other.GetComponent<CharacterController>().Move(teleportDestination);
+        }
+
+    }
 
 }
