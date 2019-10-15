d7108663ebf1c3b446c9d6b35784dff9648f80b0
Will Hart <clouduniversemc@gmail.com>
2019-10-15 11:56:19 +1100

    Abscondence - Alpha Ver. 1.3.4
    - Working Health bar
    - Polished up code
    - Added Comments
    - Preset player variable values

--- Abscondence Alpha - Project/Assets/Scripts/BottomlessPit.cs
+++ Abscondence Alpha - Project/Assets/Scripts/BottomlessPit.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BottomlessPit : MonoBehaviour
 {
-    public AITeleport aiFollower;
+    private AITeleport aiFollower;
     private Vector3 falling;
     private float fallAmount;
     PlayerController player;
@@ -12,23 +12,26 @@ public class BottomlessPit : MonoBehaviour
     [HideInInspector]
     public bool playerFallen = false;
 
-
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        aiFollower = GameObject.Find("AIFollow").GetComponent<AITeleport>();
         fallAmount = player.startingHeight + 5.0f;
     }
 
     void Update()
     {
+        // If the player falls below a certain Y level, it will teleport to the AIFollower and take damage
         if (player.transform.position.y < -fallAmount)
         {
-           // Debug.Log("Player fallen");
+            Debug.Log("Player fallen off map");
             TeleportToAI();
+            player.currentHealth -= 2; // Player takes damage upon falling into hole
             playerFallen = false;
         }
     }
 
+    // Check if the player has fallen in the pit
     void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.tag == "Player")
@@ -42,9 +45,9 @@ public class BottomlessPit : MonoBehaviour
     {
         CharacterController controller = player.GetComponent<CharacterController>();
 
-       controller.enabled = false;
-       controller.transform.SetPositionAndRotation(aiFollower.transform.position, aiFollower.transform.rotation);
-       controller.enabled = true;
+        controller.enabled = false;
+        controller.transform.SetPositionAndRotation(aiFollower.transform.position, aiFollower.transform.rotation);
+        controller.enabled = true;
         //player.GetComponent<CharacterController>().Move(aiFollower.transform.position);
     }
 }
