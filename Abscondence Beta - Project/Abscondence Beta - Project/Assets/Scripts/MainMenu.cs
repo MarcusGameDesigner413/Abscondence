8968b195f173de9c6c73f46208d1990ba9c78f9b
Connor Force <connorforcedev@gmail.com>
2019-10-28 09:37:38 +1100

    Abscondence - Beta Ver. 4.0.0
    Added Abscond Mode and touched up areas.

---
+++ Abscondence Beta - Project/Abscondence Beta - Project/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public string FirstLevelName;
+    public string AbscondLevelName;
+    public string CreditsLevelName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+    public void Play()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(FirstLevelName);
+    }
+
+    public void Abscond()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(AbscondLevelName);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void Credits()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(CreditsLevelName);
+    }
+}
