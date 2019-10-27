b137c972c950cda0c708b21f405e097630fc7de0
Connor Force <connorforcedev@gmail.com>
2019-10-28 09:59:43 +1100

    Abscondence - Beta Ver. 4.0.1
    Fixed incomplete push.

---
+++ Abscondence Beta - Project/Assets/Scripts/CreditsEnder.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
+
+public class CreditsEnder : MonoBehaviour
+{
+
+    public int timeLeft = 60; //Seconds overall
+    public Text countdown; //UI countdown timer
+    public string MainMenuName;
+
+    void Start()
+    {
+        StartCoroutine("LoseTime");
+        Time.timeScale = 1; //Setting the correct time scale
+    }
+    void Update()
+    {
+        countdown.text = ("" + timeLeft); //Showing the time on the Canvas
+
+        if (timeLeft <= 0.0f)
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(MainMenuName);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(MainMenuName);
+        }
+
+    }
+    //Coroutine
+    IEnumerator LoseTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            timeLeft--;
+        }
+
+    }
+
+}
